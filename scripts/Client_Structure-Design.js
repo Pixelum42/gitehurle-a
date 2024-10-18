b0d3d13806a25d4545fc55039ddf3ae93034875e
Pixelum <leo.carlini35@gmail.com>
2024-10-18 17:27:23 +0200

    Added -EventListeners for page links.

--- scripts/Client_Structure-Design.js
+++ scripts/Client_Structure-Design.js
@@ -1,20 +1,26 @@
 const makeNav = () => {
-    const navbar = document.createElement("nav");
+    const navbar = document.getElementById("nav-id");
     navbar.className = "navbar navbar-expand navbar-light fixed-top";
     navbar.style.backgroundColor = rootColors["--A-secondary"];
-    document.body.appendChild(navbar);
 
     const divNavbar = document.createElement("div");
     divNavbar.className = "container-fluid d-flex justify-content-evenly";
     navbar.appendChild(divNavbar);
 
+    const brandDiv = document.createElement("div");
+    brandDiv.className = "d-flex align-items-center"
+    divNavbar.appendChild(brandDiv);
+
     const brand = document.createElement("a");
     brand.className = "navbar-brand active";
     brand.setAttribute("aria-current", "page");
     brand.href = "#"
+    // roomLink0.addEventListener("click", () => {
+    // generateClient_Accueil();
+    // });
     brand.style.color = rootColors["--A-text"];
     brand.innerText = "Le Gîte Hurle";
-    divNavbar.appendChild(brand);
+    brandDiv.appendChild(brand);
 
     const logo = document.createElement("img");
     logo.className = "ms-2";
@@ -22,7 +28,7 @@ const makeNav = () => {
     logo.width = 40;
     logo.height = 40;
     logo.alt = "Logo de Le Gîte Hurle";
-    brand.appendChild(logo);
+    brandDiv.appendChild(logo);
 
     const divNavbarItems = document.createElement("div");
     divNavbar.appendChild(divNavbarItems);
@@ -38,10 +44,12 @@ const makeNav = () => {
     const dropdownTitleLink = document.createElement("a");
     dropdownTitleLink.className = "nav-link dropdown-toggle";
     dropdownTitleLink.style.color = rootColors["--A-text"];
-    dropdownTitleLink.href = "#";
     dropdownTitleLink.setAttribute("data-bs-toggle", "dropdown");
     dropdownTitleLink.setAttribute("aria-expanded", "false");
     dropdownTitleLink.innerText = "Chambres";
+    dropdownTitleLink.addEventListener('mouseover', () => {
+        dropdownTitleLink.style.cursor = 'pointer';
+    });
     dropdownTitle.appendChild(dropdownTitleLink);
 
     const dropdownMenu = document.createElement("ul");
@@ -61,8 +69,17 @@ const makeNav = () => {
     roomLink1.className = "dropdown-item";
     roomLink2.className = "dropdown-item";
     roomLink0.href = "#";
+    // roomLink0.addEventListener("click", () => {
+    //     displayRoomDetails(rooms[0]);
+    // });
     roomLink1.href = "#";
+    // roomLink1.addEventListener("click", () => {
+    //     displayRoomDetails(rooms[1]);
+    // });
     roomLink2.href = "#";
+    // roomLink2.addEventListener("click", () => {
+    //     displayRoomDetails(rooms[2]);
+    // });
     roomLink0.style.color = rootColors["--A-text"];
     roomLink1.style.color = rootColors["--A-text"];
     roomLink2.style.color = rootColors["--A-text"];
@@ -78,6 +95,9 @@ const makeNav = () => {
     const formLink0 = document.createElement("a");
     formLink0.className = "nav-link";
     formLink0.href = "#";
+    // formLink0.addEventListener("click", () => {
+    //     makeFormBook();
+    // });
     formLink0.innerText = "Réserver";
     formItem0.appendChild(formLink0);
     
@@ -86,6 +106,9 @@ const makeNav = () => {
     const formLink1 = document.createElement("a");
     formLink1.className = "nav-link";
     formLink1.href = "#";
+    // formLink1.addEventListener("click", () => {
+    //     makeFormContact();
+    // });
     formLink1.innerText = "Contact";
     formItem1.appendChild(formLink1);
 
@@ -94,13 +117,12 @@ const makeNav = () => {
 
 
 const makeHeader = () => {
-    const header = document.createElement("header");
+    const header = document.getElementById("header-id");
     header.className = "py-5";
     header.style.background = "url('https://www.gites-de-france.com/sites/default/files/styles/facebook_1200x630/public/ispagnac-lozere.jpeg') bottom";
     header.style.backgroundSize = "cover";
     header.style.backgroundRepeat = "no-repeat";
     header.style.backgroundAttachment = "fixed";
-    document.body.appendChild(header);
 
     const headerDiv = document.createElement("div");
     headerDiv.className = "my-5 text-white text-center";
@@ -109,7 +131,7 @@ const makeHeader = () => {
     const h1Tag = document.createElement("h1");
     h1Tag.className = "display-2 py-3 my-5 fw-bold";
     h1Tag.style.textShadow = "1px 3px 0 #969696, 1px 13px 5px #aba8a8";
-    h1Tag.style.color = "var(--A-accent)"
+    h1Tag.style.color = rootColors["--A-accent"];
     h1Tag.innerText = "Le Gîte Hurle";
 
     headerDiv.appendChild(h1Tag);
@@ -117,9 +139,8 @@ const makeHeader = () => {
 
 
 const makeFooter = () => {
-    const footer = document.createElement("footer");
+    const footer = document.getElementById("footer-id");
     footer.className = "container";
-    document.body.appendChild(footer);
 
     const footerDiv = document.createElement("div");
     footerDiv.className = "d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top";
@@ -137,6 +158,9 @@ const makeFooter = () => {
     const footerLogoLink = document.createElement("a");
     footerLogoLink.className = "ms-3";
     footerLogoLink.href = "#";
+    // formLink1.addEventListener("click", () => {
+    //     adminPage();
+    // });
     footerCopyrightDiv.appendChild(footerLogoLink);
 
     const footerLogo = document.createElement("img");
@@ -163,20 +187,28 @@ const makeFooter = () => {
 }
 
 
-
-const generateStructure = () => {
-    document.body.className = "d-flex flex-column mt-5 pt-3";
-    document.body.style.backgroundColor = rootColors["--A-background"];
-
-    makeNav();
-    makeHeader();
-    // Add page function here
-    makeFooter();
-
+function defineElementColors() {
     // Defines color for specific elements
     const allATag = document.querySelector("a");
     allATag.style.color = rootColors["--A-text"];
+    
+    // allATag.addEventListener("mouseenter", )
+    // allATag.addEventListener("mouseleave")
+    // allATag.style.backgroundColor = rootColors["--A-background"];
+
+    document.body.className = "d-flex flex-column mt-5 pt-2";
+    document.body.style.backgroundColor = rootColors["--A-background"];
+    
     const contactLink = document.getElementById("contact");
     contactLink.style.color = rootColors["--A-text"];
+}
+
+
+const generateStructure = () => {
+    makeNav();
+    makeHeader();
+    makeFooter();
+
+    defineElementColors();
 }
 generateStructure();
