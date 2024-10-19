f335c3b61dc6870bc0d6d8d1d6cb8fa4dfc030d8
sotoludo <sotoludo@gmail.com>
2024-10-20 00:53:27 +0200

    minor fix

--- scripts/Client_Structure-Design.js
+++ scripts/Client_Structure-Design.js
@@ -4,22 +4,28 @@ const adminLog = {
   }
 
 const makeNav = () => {
-    const navbar = document.getElementById('nav-id');
+    const navbar = document.getElementById("nav-id");
     navbar.className = "navbar navbar-expand navbar-light fixed-top";
     navbar.style.backgroundColor = rootColors["--A-secondary"];
-    document.body.appendChild(navbar);
 
     const divNavbar = document.createElement("div");
     divNavbar.className = "container-fluid d-flex justify-content-evenly";
     navbar.appendChild(divNavbar);
 
+    const brandDiv = document.createElement("div");
+    brandDiv.className = "d-flex align-items-center"
+    divNavbar.appendChild(brandDiv);
+
     const brand = document.createElement("a");
     brand.className = "navbar-brand active";
     brand.setAttribute("aria-current", "page");
     brand.href = "#"
+    // roomLink0.addEventListener("click", () => {
+    // generateClient_Accueil();
+    // });
     brand.style.color = rootColors["--A-text"];
     brand.innerText = "Le Gîte Hurle";
-    divNavbar.appendChild(brand);
+    brandDiv.appendChild(brand);
 
     const logo = document.createElement("img");
     logo.className = "ms-2";
@@ -27,7 +33,7 @@ const makeNav = () => {
     logo.width = 40;
     logo.height = 40;
     logo.alt = "Logo de Le Gîte Hurle";
-    brand.appendChild(logo);
+    brandDiv.appendChild(logo);
 
     const divNavbarItems = document.createElement("div");
     divNavbar.appendChild(divNavbarItems);
@@ -43,10 +49,12 @@ const makeNav = () => {
     const dropdownTitleLink = document.createElement("a");
     dropdownTitleLink.className = "nav-link dropdown-toggle";
     dropdownTitleLink.style.color = rootColors["--A-text"];
-    dropdownTitleLink.href = "#";
     dropdownTitleLink.setAttribute("data-bs-toggle", "dropdown");
     dropdownTitleLink.setAttribute("aria-expanded", "false");
     dropdownTitleLink.innerText = "Chambres";
+    dropdownTitleLink.addEventListener('mouseover', () => {
+        dropdownTitleLink.style.cursor = 'pointer';
+    });
     dropdownTitle.appendChild(dropdownTitleLink);
 
     const dropdownMenu = document.createElement("ul");
@@ -66,8 +74,17 @@ const makeNav = () => {
     roomLink1.className = "dropdown-item";
     roomLink2.className = "dropdown-item";
     roomLink0.href = "#";
+    // roomLink0.addEventListener("click", () => {
+    //     displayRoomDetails(rooms[0]);
+    // });
     roomLink1.href = "#";
+    // roomLink1.addEventListener("click", () => {
+    //     displayRoomDetails(rooms[1]);
+    // });
     roomLink2.href = "#";
+    // roomLink2.addEventListener("click", () => {
+    //     displayRoomDetails(rooms[2]);
+    // });
     roomLink0.style.color = rootColors["--A-text"];
     roomLink1.style.color = rootColors["--A-text"];
     roomLink2.style.color = rootColors["--A-text"];
@@ -83,6 +100,9 @@ const makeNav = () => {
     const formLink0 = document.createElement("a");
     formLink0.className = "nav-link";
     formLink0.href = "#";
+    // formLink0.addEventListener("click", () => {
+    //     makeFormBook();
+    // });
     formLink0.innerText = "Réserver";
     formItem0.appendChild(formLink0);
     
@@ -91,6 +111,9 @@ const makeNav = () => {
     const formLink1 = document.createElement("a");
     formLink1.className = "nav-link";
     formLink1.href = "#";
+    // formLink1.addEventListener("click", () => {
+    //     makeFormContact();
+    // });
     formLink1.innerText = "Contact";
     formItem1.appendChild(formLink1);
 
@@ -99,13 +122,12 @@ const makeNav = () => {
 
 
 const makeHeader = () => {
-    const header = document.getElementById('header-id');
+    const header = document.getElementById("header-id");
     header.className = "py-5";
     header.style.background = "url('https://www.gites-de-france.com/sites/default/files/styles/facebook_1200x630/public/ispagnac-lozere.jpeg') bottom";
     header.style.backgroundSize = "cover";
     header.style.backgroundRepeat = "no-repeat";
     header.style.backgroundAttachment = "fixed";
-    document.body.appendChild(header);
 
     const headerDiv = document.createElement("div");
     headerDiv.className = "my-5 text-white text-center";
@@ -114,92 +136,11 @@ const makeHeader = () => {
     const h1Tag = document.createElement("h1");
     h1Tag.className = "display-2 py-3 my-5 fw-bold";
     h1Tag.style.textShadow = "1px 3px 0 #969696, 1px 13px 5px #aba8a8";
-    h1Tag.style.color = "var(--A-accent)"
+    h1Tag.style.color = rootColors["--A-accent"];
     h1Tag.innerText = "Le Gîte Hurle";
 
     headerDiv.appendChild(h1Tag);
 }
-
-
-const makeFooter = () => {
-    const footer = document.getElementById('footer-id');
-    footer.className = "container";
-    document.body.appendChild(footer);
-
-    const footerDiv = document.createElement("div");
-    footerDiv.className = "d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top";
-    footer.appendChild(footerDiv);
-
-    const footerCopyrightDiv = document.createElement("div");
-    footerCopyrightDiv.className = "col-md-4 d-flex align-items-center";
-    footerDiv.appendChild(footerCopyrightDiv);
-
-    const footerCopyright = document.createElement("span");
-    footerCopyright.className = "text-body-secondary";
-    footerCopyright.innerText = "© 2024 Le Gîte Hurle";
-    footerCopyrightDiv.appendChild(footerCopyright);
-
-    const footerLogoLink = document.createElement("a");
-    footerLogoLink.className = "ms-3 co-div";
-    footerLogoLink.href = "#";
-    footerLogoLink.addEventListener('click',()=>{
-      const blurTable=[];
-        blurTable.push(document.getElementById('header-id'));
-        blurTable.push(document.getElementById('nav-id'));
-        blurTable.push(document.getElementById('main-id'));
-        blurTable.push(document.getElementById('footer-id'));
-         
-         console.log(blurTable);
-         for (let i = 0; i < blurTable.length; i++) {
-          blurTable[i].style.filter= 'blur(4px)';          
-         }
-          generateConnexion();
-    });
-    footerCopyrightDiv.appendChild(footerLogoLink);
-
-    const footerLogo = document.createElement("img");
-    footerLogo.src = "assets/logo.svg";
-    footerLogo.className='co-div';
-    footerLogo.width = 40;
-    footerLogo.height = 40;
-    footerLogo.alt = "logo";
-    footerLogoLink.appendChild(footerLogo);
-
-    const contactUl = document.createElement("ul");
-    contactUl.className = "nav col-md-4 justify-content-end";
-    footerDiv.appendChild(contactUl);
-
-    const contactItem = document.createElement("li");
-    contactItem.className = "nav-item";
-    contactUl.appendChild(contactItem);
-
-    const contactLink = document.createElement("a");
-    contactLink.className = "nav-link";
-    contactLink.id = "contact";
-    contactLink.href = "#";
-    contactLink.innerText = "Contact";
-    contactItem.appendChild(contactLink);
-}
-
-
-
-const generateStructure = () => {
-    document.body.className = "d-flex flex-column mt-5 pt-3";
-    document.body.style.backgroundColor = rootColors["--A-background"];
-
-    makeNav();
-    makeHeader();
-    // Add page function here
-    makeFooter();
-
-    // Defines color for specific elements
-    const allATag = document.querySelector("a");
-    allATag.style.color = rootColors["--A-text"];
-    const contactLink = document.getElementById("contact");
-    contactLink.style.color = rootColors["--A-text"];
-}
-
-
 
 function generateConnexion() {
     if(document.getElementById('co-div')){document.getElementById('co-div').remove();}
@@ -273,8 +214,6 @@ function generateConnexion() {
     button.className = "co-div btn btn-primary w-100 py-2";
     button.type = "submit";
     button.textContent = "Sign in";
-    // button.setAttribute('data-bs-toggle',"modal");
-    // button.setAttribute('data-bs-target',"#error-modal");
     button.addEventListener('click',adminCo);
     form.appendChild(button); 
   }
@@ -308,57 +247,94 @@ function generateConnexion() {
       //generateAdminResa();
     }
     else{
-      
-      alert('Mauvais Utilisateur / Mot de Passe');
-      // const modal = document.createElement('div');
-      // document.body.appendChild(modal);
-      // modal.classList.add('modal','fade');
-      // modal.id="error-modal";
-      // modal.tabIndex="-1";
-      // modal.style.zIndex='999';
-      // modal.setAttribute('aria-labelledby','error-modal-label');
-      // modal.setAttribute('aria-hidden','true');
-  
-      // const modalDialog = document.createElement('div');
-      // modalDialog.className='modal-dialog';
-      // modal.appendChild(modalDialog);
-      
-      // const modalContent = document.createElement('div');
-      // modalContent.className='modal-content';
-      // modalDialog.appendChild(modalContent);
-  
-      // const modalHeader = document.createElement('div');
-      // modalHeader.className='modal-header';
-      // const modalBody = document.createElement('div');
-      // modalBody.className='modal-body';
-      // modalBody.textContent="Mauvais Mot de Passe / Nom d'Utilisateur";
-      // const modalFooter = document.createElement('div');
-      // modalFooter.className='modal-footer';
-      // modalContent.append(modalHeader,modalBody,modalFooter);
-  
-      // const mHH1 = document.createElement('h1');
-      // mHH1.classList.add('modal-title','fs-5');
-      // mHH1.id='title-modal-label';
-      // mHH1.textContent='Erreur';
-      // const mHB = document.createElement('button');
-      // mHB.type='button';
-      // mHB.className='btn-close';
-      // mHB.setAttribute('data-bs-dismiss','modal');
-      // mHB.setAttribute('aria-label','Close');
-      // modalHeader.append(mHH1,mHB);
-  
-  
-      // const mFB = document.createElement('button');
-      // mFB.type='button';
-      // mFB.classList.add('btn','btn-secondary');
-      // mFB.setAttribute('data-bs-dismiss','modal');
-      // mFB.textContent='Close';
-      // modalFooter.appendChild(mFB);
-      
+      alert('Mauvais Utilisateur / Mot de Passe');      
     }
   }
 
+const makeFooter = () => {
+    const footer = document.getElementById("footer-id");
+    footer.className = "container";
+
+    const footerDiv = document.createElement("div");
+    footerDiv.className = "d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top";
+    footer.appendChild(footerDiv);
+
+    const footerCopyrightDiv = document.createElement("div");
+    footerCopyrightDiv.className = "col-md-4 d-flex align-items-center";
+    footerDiv.appendChild(footerCopyrightDiv);
+
+    const footerCopyright = document.createElement("span");
+    footerCopyright.className = "text-body-secondary";
+    footerCopyright.innerText = "© 2024 Le Gîte Hurle";
+    footerCopyrightDiv.appendChild(footerCopyright);
+
+    const footerLogoLink = document.createElement("a");
+    footerLogoLink.className = "ms-3 co-div";
+    footerLogoLink.href = "#";
+    
+    footerLogoLink.addEventListener('click',()=>{
+        const blurTable=[];
+        blurTable.push(document.getElementById('header-id'));
+        blurTable.push(document.getElementById('nav-id'));
+        blurTable.push(document.getElementById('main-id'));
+        blurTable.push(document.getElementById('footer-id'));
+        for (let i = 0; i < blurTable.length; i++) {
+        blurTable[i].style.filter= 'blur(4px)';          
+        }  
+        generateConnexion();
+      });
+    // formLink1.addEventListener("click", () => {
+    //     adminPage();
+    // });
+    footerCopyrightDiv.appendChild(footerLogoLink);
+
+    const footerLogo = document.createElement("img");
+    footerLogo.src = "assets/logo.svg";
+    footerLogo.className='co-div';
+    footerLogo.width = 40;
+    footerLogo.height = 40;
+    footerLogo.alt = "logo";
+    footerLogoLink.appendChild(footerLogo);
+
+    const contactUl = document.createElement("ul");
+    contactUl.className = "nav col-md-4 justify-content-end";
+    footerDiv.appendChild(contactUl);
+
+    const contactItem = document.createElement("li");
+    contactItem.className = "nav-item";
+    contactUl.appendChild(contactItem);
+
+    const contactLink = document.createElement("a");
+    contactLink.className = "nav-link";
+    contactLink.id = "contact";
+    contactLink.href = "#";
+    contactLink.innerText = "Contact";
+    contactItem.appendChild(contactLink);
+}
 
 
+function defineElementColors() {
+    // Defines color for specific elements
+    const allATag = document.querySelector("a");
+    allATag.style.color = rootColors["--A-text"];
+    
+    // allATag.addEventListener("mouseenter", )
+    // allATag.addEventListener("mouseleave")
+    // allATag.style.backgroundColor = rootColors["--A-background"];
 
+    document.body.className = "d-flex flex-column mt-5 pt-2";
+    document.body.style.backgroundColor = rootColors["--A-background"];
+    
+    const contactLink = document.getElementById("contact");
+    contactLink.style.color = rootColors["--A-text"];
+}
+
+
+const generateStructure = () => {
+    makeNav();
+    makeHeader();
+    makeFooter();
+
+    defineElementColors();
+}
 generateStructure();
